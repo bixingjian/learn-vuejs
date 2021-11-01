6875f902f3b206c9b6d1eecf33c463b3676f95bb
bixingjian <bixingjian19@163.com>
2021-11-01 13:40:18 +0800

    组合api实现加减器

---
+++ geek-admin/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="subtract">-</button>
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let count = ref(1);
+function add() {
+  count.value++;
+}
+function subtract() {
+  count.value--;
+}
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
--- geek-admin/src/main.js
+++ geek-admin/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import router from "./router/index";
+
+// 会在index.html中被引入, 将app, router....整合, 然后mount(相当于render))在id为app的div上
 createApp(App).use(router).mount("#app");
